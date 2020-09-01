5d7b813cd9b67c182e56fa1d019c9d5021920e0a
natagoh <natagoh@stanford.edu>
2020-08-31 20:23:46 -0400

    cube

--- src/engine/Window.java
+++ src/engine/Window.java
@@ -6,6 +6,7 @@ import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.glClearColor;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -88,6 +89,7 @@ public class Window {
 
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glEnable(GL_DEPTH_TEST);
     }
 
     public void setClearColor(float r, float g, float b, float alpha) {
--- src/game/Clonecraft.java
+++ src/game/Clonecraft.java
@@ -1,8 +1,9 @@
 package game;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.glViewport;
+
+import org.joml.Vector3f;
 
 import engine.GameItem;
 import engine.IGameLogic;
@@ -15,6 +16,10 @@ public class Clonecraft implements IGameLogic {
     private float color = 0.0f;
     private final Renderer renderer;
     private GameItem[] gameItems;
+	private int displyInc;
+	private int displzInc;
+	private int displxInc;
+	private int scaleInc;
     
     public Clonecraft() {
         renderer = new Renderer();
@@ -23,20 +28,36 @@ public class Clonecraft implements IGameLogic {
     @Override
     public void init(Window window) throws Exception {
         renderer.init(window);
+
         float[] positions = new float[] {
-			-0.5f, 0.5f, 0.0f,
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.5f, 0.5f, 0.0f,
+			-0.5f, 0.5f, 0.5f,		// VO	
+			-0.5f, -0.5f, 0.5f,		// V1
+			0.5f, -0.5f, 0.5f,		// V2
+			0.5f, 0.5f, 0.5f,		// V3
+			-0.5f, 0.5f, -0.5f,		// V4
+			0.5f, 0.5f, -0.5f,		// V5
+			-0.5f, -0.5f, -0.5f,	// V6
+			0.5f, -0.5f, -0.5f,		// V7
 		};
+
         float[] colors = new float[]{
-        		0.5f, 0.0f, 0.0f,
-        		0.0f, 0.5f, 0.0f,
-        		0.0f, 0.0f, 0.5f,
-        		0.0f, 0.5f, 0.5f,
-        		};
+    		0.5f, 0.0f, 0.0f,
+    		0.0f, 0.5f, 0.0f,
+    		0.0f, 0.0f, 0.5f,
+    		0.0f, 0.5f, 0.5f,
+    		0.5f, 0.0f, 0.0f,
+    		0.0f, 0.5f, 0.0f,
+    		0.0f, 0.0f, 0.5f,
+    		0.0f, 0.5f, 0.5f,
+    	};
+
 		int[] indices = new int[] {
-			0, 1, 3, 3, 1, 2,
+			0, 1, 3, 3, 1, 2, 	// Front face
+			4, 0, 3, 5, 4, 3,	// Top Face
+			3, 2, 7, 5, 3, 7,	// Right face
+			6, 1, 0, 6, 0, 4,	// Left face
+			2, 1, 6, 2, 6, 7,	// Bottom face
+			7, 6, 4, 7, 4, 5,	// Back face
 		};
 		Mesh mesh = new Mesh(positions, colors, indices);
         GameItem gameItem = new GameItem(mesh);
@@ -46,22 +67,53 @@ public class Clonecraft implements IGameLogic {
     
     @Override
     public void input(Window window) {
-        if ( window.isKeyPressed(GLFW_KEY_UP) ) {
-            direction = 1;
-        } else if ( window.isKeyPressed(GLFW_KEY_DOWN) ) {
-            direction = -1;
-        } else {
-            direction = 0;
+        displyInc = 0;
+        displxInc = 0;
+        displzInc = 0;
+        scaleInc = 0;
+        if (window.isKeyPressed(GLFW_KEY_UP)) {
+            displyInc = 1;
+        } else if (window.isKeyPressed(GLFW_KEY_DOWN)) {
+            displyInc = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_LEFT)) {
+            displxInc = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_RIGHT)) {
+            displxInc = 1;
+        } else if (window.isKeyPressed(GLFW_KEY_A)) {
+            displzInc = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_Q)) {
+            displzInc = 1;
+        } else if (window.isKeyPressed(GLFW_KEY_Z)) {
+            scaleInc = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_X)) {
+            scaleInc = 1;
         }
     }
 
     @Override
     public void update(float interval) {
-        color += direction * 0.01f;
-        if (color > 1) {
-            color = 1.0f;
-        } else if ( color < 0 ) {
-            color = 0.0f;
+    	for (GameItem gameItem : gameItems) {
+            // Update position
+            Vector3f itemPos = gameItem.getPosition();
+            float posx = itemPos.x + displxInc * 0.01f;
+            float posy = itemPos.y + displyInc * 0.01f;
+            float posz = itemPos.z + displzInc * 0.01f;
+            gameItem.setPosition(posx, posy, posz);
+
+            // Update scale
+            float scale = gameItem.getScale();
+            scale += scaleInc * 0.05f;
+            if (scale < 0) {
+                scale = 0;
+            }
+            gameItem.setScale(scale);
+
+            // Update rotation angle
+            float rotation = gameItem.getRotation().x + 1.5f;
+            if (rotation > 360) {
+                rotation = 0;
+            }
+            gameItem.setRotation(rotation, rotation, rotation);
         }
     }
 
